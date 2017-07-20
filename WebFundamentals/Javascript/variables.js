ed9ff859b7be76b0918996b7c2797b2d36e6081a
Ty <TSBukowski@yahoo.com>
2017-07-19 22:30:24 -0500

    added variable.html and variables.js

---
+++ WebFundamentals/Javascript/variables.js
@@ -0,0 +1,25 @@
+    var myAge = 29;
+    var currentYear = 2017;
+    var tooCold = -1;
+    var pi = 3.14159265359;
+    var myName = 'Ty';
+    var myTown = 'Wichita Falls';
+    var favoriteMovie = 'Bad Boys 2';
+    var isHotOutside = true;
+    var isCorrect = 1 === 2;
+    var caughtUp = undefined;
+
+    
+    function sayHello() {
+        console.log('I am:', myAge);
+        console.log('The year is:', currentYear);
+        console.log(tooCold + 'deg', 'is way too cold!');
+        console.log('Pi goes', pi, 'and on and on and on');
+        console.log('Hi! My name is', myName);
+        console.log("I've lived in", myTown, 'since 1993');
+        console.log('My favorite movie is', favoriteMovie);
+        console.log(isHotOutside);
+        console.log(isCorrect);
+        console.log(caughtUp);
+    }
+ sayHello();
