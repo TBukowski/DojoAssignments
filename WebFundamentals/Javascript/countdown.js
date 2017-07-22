50db6ab559e47b441281db18b67195c1faab816d
Ty <TSBukowski@yahoo.com>
2017-07-22 11:02:17 -0500

    added countdown.html and countdown.js

---
+++ WebFundamentals/Javascript/countdown.js
@@ -0,0 +1,17 @@
+for (var daysTilBirthday = 174; daysTilBirthday > 30; daysTilBirthday--)
+{
+    console.log('My birthday is no where to be seen,', daysTilBirthday, 'days away..');
+}
+for (daysTilBirthday; daysTilBirthday > 5; daysTilBirthday--)
+{
+    console.log('', daysTilBirthday, 'days until my birthday, I see it off in the distance!');
+}
+for (daysTilBirthday; daysTilBirthday > 0; daysTilBirthday--)
+{
+    console.log('THE COUNTDOWN IS HERE!!', daysTilBirthday);
+}
+if (daysTilBirthday === 0) {
+    console.log('I AM GOING TO PARTY LIKE ITS MY BIRTHDAY, BECAUSE IT IS!!!!');
+} else {
+    console.log('Where did my birthday go?')
+}
