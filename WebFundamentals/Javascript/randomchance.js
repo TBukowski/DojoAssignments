6575d0c55a2fc6737dba3227d9105f9f3f05827b
Ty <TSBukowski@yahoo.com>
2017-07-24 19:54:51 -0500

    updated fancyarray.html, added randomchance.html, and randomchance.js

---
+++ WebFundamentals/Javascript/randomchance.js
@@ -0,0 +1,34 @@
+// Random Chance
+// Let's play the slots!
+
+// Make a function that takes a number of quarters (1 quarter = 1 game).
+
+// There is a 1 in 100 chance to win the slot machine (which will give you between 
+// 50 - 100 quarters -- use Math.random and Math.floor to pick a random number of coins).
+
+// While the user still has quarters, use Math.random to determine if they won.
+
+// If they ever win, return the number of quarters (ex: they had 50 remaining quarters and 
+// won 90, so it should return 140).
+
+// Return 0 if all the quarters were wasted.
+
+// Bonus (Only If You Have Time):  Let the user pass the number they are willing to leave with
+// (ex: If they won the lottery and still have 40 coins, they will keep playing until they 
+// have collected 200 coins)
+
+function slotMachine(quarters){
+  while(quarters){
+    var win = Math.random();
+    console.log(win)
+    if(win < .01){
+    (Math.floor(Math.random()*50)+50)
+    var winnings = quarters + Math.floor(Math.random()*50)+50
+    console.log(winnings)
+    return winnings
+    } 
+    quarters--; 
+  }
+  return 0 
+}
+console.log(slotMachine(50));
